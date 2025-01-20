7a1a21fd9f864d7110050e14085f88c1a11151d8
luan048 <luanmonteiropedrosa@gmail.com>
2025-01-20 14:52:26 -0300

    Add Security in Reservation

--- services/reservationServices.js
+++ services/reservationServices.js
@@ -22,15 +22,15 @@ async function insertReservation(namerestaurant, nameperson, numbertable, quantp
     try{
         await cliente.connect()
 
-        const result= await cliente.query("SELECT * FROM restaurant WHERE restaurantname = '"+namerestaurant+"';")
+        const result= await cliente.query(`SELECT * FROM restaurant WHERE restaurantname = $1`, [namerestaurant])
 
         if(result.rows.length > 0) {
-            const resultUser = await cliente.query("SELECT * FROM users WHERE username = '"+nameperson+"';")
+            const resultUser = await cliente.query(`SELECT * FROM users WHERE username = $1`, [nameperson])
             
             if(resultUser.rows.length > 0) {
                 const id_reservation = Math.floor(10000 + Math.random() * 90000)
 
-                await cliente.query('INSERT INTO reservation("id_reservation", "namerestaurant", "nameperson", "numbertable", "quantperson", "hour") values (' + "'"+id_reservation+"', '" + namerestaurant + "', '" + nameperson + "', '" + numbertable + "', '" + quantperson + "', '"+hour+"');")
+                await cliente.query(`INSERT INTO reservation($1, $2, $3, $4, $5, $6)`, [id_reservation, namerestaurant, nameperson, numbertable, quantperson, hour])
                 console.log('Valor inserido na tabela')
 
                 const resultado = await cliente.query('SELECT * FROM reservation')
@@ -61,7 +61,7 @@ async function delReservation(id_reservation) {
     try{
         await cliente.connect()
 
-        await cliente.query("DELETE FROM reservation where id_reservation = '"+id_reservation+"';")
+        await cliente.query(`DELETE FROM reservation where id_reservation = $1`, [id_reservation])
         console.log('Valor removido da tabela')
 
         const resultado = await cliente.query('SELECT * FROM reservation')
@@ -82,7 +82,7 @@ async function upReservation(idreservation, newId, newRestaurant, newPerson, new
     try{
         await cliente.connect()
 
-        await cliente.query("UPDATE reservation SET idreservation = '"+newId+ "',namerestaurant = '" + newRestaurant + "', nameperson = '"+newPerson+ "', numbertable = '" + newTable + "' WHERE idreservation = '" + idreservation + "';")
+        await cliente.query(`UPDATE reservation SET idreservation = $1, namerestaurant = $2, nameperson  = $3, numbertable = $4 WHERE idreservation = $5`, [newId, newRestaurant, newPerson, newTable, idreservation])
 
         const resultado = await cliente.query('SELECT * FROM reservation')
         console.table(resultado.rows)
